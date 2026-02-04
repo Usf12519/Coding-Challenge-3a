9bbd93c7316d6b36be0afbcda125d3b7f1bf92e0
Cameron Smith <Cs11@usf.edu>
2026-02-04 17:50:50 -0500

    Added new Customer

--- cc_3a.js
+++ cc_3a.js
@@ -1,26 +1,29 @@
 // Step2: Create Customer Objects
-let customers = ["Alice","Brandon","Cameron"];
-
-let customer =  {
+let customers = [{
     name: 'Alice',
     email: 'alice1234@example.com',
     purchases: ["labtop","phone","playstation"],
-};
-
-let customer = {
+},
+ {
     name: 'Brandon',
     email: 'brandon456@example.com',
     purchases: ["apples","bananas","carrots"],
-};
-
-let customer = {
-    name: 'Cameron',
+},
+{
+name: 'Cameron',
     email: 'Cameron789@example.com',
     purchases: ["eggs","ice cream","pizza"],
-};
+}];
 
 // Step 3: Add and Remove Data
 
-let customers = ["Alice","Brandon","Cameron"];
-customer.push("Daniel");
-console.log(customers);
+let newCustomer = {
+    name:'Daniel'
+};
+
+customers.push(newCustomer);
+console.log(newCustomer);
+customers.shift();
+
+//
+
